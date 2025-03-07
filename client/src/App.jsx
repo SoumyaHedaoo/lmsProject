5ef7ca6537e68acfd3d68c09a35d1b5242421a07
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-03-07 19:59:50 +0530

    handle navbar display and home page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import React from 'react'
+import { Routes , Route } from 'react-router-dom';
+import Home from './pages/student/Home';
+import Navbar from './components/student/Navbar';
 
 function App() {
 
   return (
-    <>
+    <div className='text-default min-h-screen bg-white' >
 
-    </>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
