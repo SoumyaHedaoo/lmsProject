cfafeb754d9499d8db831f8f1b032d3973afeb69
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-03-07 22:50:38 +0530

    changes to make navbar responsive

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -9,16 +9,45 @@ const Navbar = () => {
 
   return (
     <div className={`flex item-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-        <img src={assets.logo} alt="Logo" className="w-16 h-16 object-contain rounded-full  lg:w-32 cursor-pointer " />
-        <div classname ='hidden md:flex items-center gap-5 text-grey-500'>
-            <div>
-                <button> Become Educator</button>
-                | <Link to='/my-enrollments'> My Enrollements</Link>
+        <img src={assets.logo} alt="Logo" className="w-28  lg:w-32 cursor-pointer " />
+        <div className ='hidden md:flex items-center gap-5 text-grey-500'>
+            <div className='flex items-center gap-5 '>
+                <button> Become Educator</button>  |  
+                <Link to='/my-enrollments'> My Enrollements</Link>
+
             </div>
-            <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+            <button className='bg-blue-600 text-white px-5 py-2 rounded-full '>Create Account</button>
+    
+        </div>
+
+        <div className='md:hidden flex item-center gap-2 sm:gap-5 text-gray-500'>
+            <div>
+            <button> Become Educator</button>  |  
+            <Link to='/my-enrollments'> My Enrollements</Link>
+            <button><img src={assets.user_icon} alt="Default_user" /></button>
+            </div>
         </div>
     </div>
   )
 }
 
 export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
