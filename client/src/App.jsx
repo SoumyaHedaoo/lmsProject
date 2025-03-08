501ea48370f18cedee01514b8250f1043516bf7b
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-03-08 13:56:01 +0530

    Changes to show diff navbar on educator route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
 import React from 'react'
-import { Routes , Route } from 'react-router-dom';
+import { Routes , Route, useMatch } from 'react-router-dom';
 import Home from './pages/student/Home';
 import Navbar from './components/student/Navbar';
+import AddCourse from './pages/educator/AddCourse';
+import Educator from './pages/educator/Educator';
+import Dashboard from './pages/educator/Dashboard';
 
 function App() {
+
+  const isEducator = useMatch('/educator/*');
 
   return (
     <div className='text-default min-h-screen bg-white' >
 
-    <Navbar />
+    {!isEducator && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/educator' element={<Educator />}>
+          <Route path='educator' element={<Dashboard />} />
+          <Route path='add-course' element={<AddCourse />} />
+        </Route>
       </Routes>
     </div>
   )
