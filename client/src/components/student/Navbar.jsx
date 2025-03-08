a29daf55d2fdde084d5345bf9c0ceaaa179c16a2
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-03-08 14:01:43 +0530

    steup Clerk Authentication for for both window and mobile

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { assets } from '../../assets/assets';;
+import { assets } from '../../assets/assets';
+import { useClerk , UserButton , useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
     const isCourseListPage = location.pathname.includes('/CourseList');
+
+    const {openSignIn} =useClerk();
+    const {user} = useUser();
+
+
+
 
 
   return (
@@ -12,19 +19,23 @@ const Navbar = () => {
         <img src={assets.logo} alt="Logo" className="w-28  lg:w-32 cursor-pointer " />
         <div className ='hidden md:flex items-center gap-5 text-grey-500'>
             <div className='flex items-center gap-5 '>
-                <button> Become Educator</button>  |  
+                
+                {user && <>
+                    <button> Become Educator</button>  |  
                 <Link to='/my-enrollments'> My Enrollements</Link>
+                </>}
 
             </div>
-            <button className='bg-blue-600 text-white px-5 py-2 rounded-full '>Create Account</button>
+            {user ? <UserButton/> : 
+            <button onClick={()=>{openSignIn()}} className='bg-blue-600 text-white px-5 py-2 rounded-full cursor-grab'>Create Account</button>}
     
         </div>
 
         <div className='md:hidden flex item-center gap-2 sm:gap-5 text-gray-500'>
-            <div>
-            <button> Become Educator</button>  |  
-            <Link to='/my-enrollments'> My Enrollements</Link>
-            <button><img src={assets.user_icon} alt="Default_user" /></button>
+            <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+            {user && <><button> Become Educator</button>  |  
+            <Link to='/my-enrollments'> My Enrollements</Link> </>}
+            {user ? <UserButton/> : <button onClick={()=>{openSignIn()}}><img src={assets.user_icon} alt="Default_user" /></button>}
             </div>
         </div>
     </div>
