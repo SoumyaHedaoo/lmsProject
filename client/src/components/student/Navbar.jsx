f8e3aaa1b46a2a2a5cb4487809d040d845b80d7a
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-03-14 13:57:17 +0530

    clikable logo

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from "react-router-dom";
 import { assets } from '../../assets/assets';
 import { useClerk , UserButton , useUser } from '@clerk/clerk-react';
+import { AppContext } from '../../context/AppContext';
 
 const Navbar = () => {
 
@@ -10,13 +11,13 @@ const Navbar = () => {
     const {openSignIn} =useClerk();
     const {user} = useUser();
 
-
+    const {navigate} = useContext(AppContext);
 
 
 
   return (
     <div className={`flex item-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-        <img src={assets.logo} alt="Logo" className="w-28  lg:w-32 cursor-pointer " />
+        <img src={assets.logo} alt="Logo" className="w-28  lg:w-32 cursor-pointer " onClick={()=> navigate('/')}/>
         <div className ='hidden md:flex items-center gap-5 text-grey-500'>
             <div className='flex items-center gap-5 '>
                 
