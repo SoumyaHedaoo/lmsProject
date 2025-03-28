2a88a902461d936dcf5f1c1c044453d726b50793
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-03-29 01:10:59 +0530

    wrote helperf unction for course detail page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import {createContext, useEffect, useState} from 'react';
 import { dummyCourses } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration';
 
 export const AppContext = createContext();
 
@@ -27,13 +28,44 @@ export const AppContextProvider =(props)=>{
         return totalRating/course.courseRatings.length;
 
     }
+
+    //function to cal chapter time
+
+    const calChapterTime = (chapter)=>{
+        let time=0;
+        chapter.chapterContent.map((lecture)=> time+=lecture.lectureDuration)
+        return humanizeDuration(time*60*1000 , {units :["h" , "m"]})
+    }
+
+    //function to cal course duration
+
+    const calCourseDuration=(course)=>{
+        let time=0;
+
+        course.courseContent.map((capter)=> chapter.chapterContent.map((lecture)=>time +=lecture.lectureDuration))
+
+        return humanizeDuration(time*60*1000 , {units: ["h" , "m"]})
+    }
+
+    //function to cal num of lecture in course
+
+    const calLecCount =(course)=>{
+        let totalLectures =0;
+
+        course.courseContent.forEach(chapter =>{
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures+=chapter.chapterContent.length
+            }
+        });
+        return totalLectures;
+    }
     
     useEffect(()=>{
         fetchAllCourses();
     } , [])
 
     const value = {
-        allCourses , navigate , calculateRating , isEducator , setIsEducator
+        allCourses , navigate , calculateRating , isEducator , setIsEducator , calChapterTime , calCourseDuration , calLecCount
     }
     
     return (
