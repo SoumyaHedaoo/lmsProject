0f2f242f8afae87db00dd232e231c7929b0ef449
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-04-01 19:27:10 +0530

    Designed educator's studentEnrolled page

---
+++ client/src/pages/educator/StudentEnrolled.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import { dummyStudentEnrolled } from "../../assets/assets";
+import Loading from "../../components/student/Loading";
+
+const StudentEnrolled = () => {
+  const [data, setData] = useState(null);
+
+  const fetchdata = async () => {
+    setData(dummyStudentEnrolled);
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, [data]);
+
+  return data ? (
+    <div className="h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <div className="w-full">
+        <h2 className="pb-4 text-lg font-medium"> Student Enrolled</h2>
+        <div className="flex -flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20">
+          <table className="md:table-auto table-fixed w-full overflow-hidden ">
+            <thead className="text-gray-900 border-b border-gray-500/20 text-sm text-left">
+              <tr>
+                <th className="px-4 py-3 font-semibold text-center hidden sm:table-cell">
+                  #
+                </th>
+                <th className="px-4 py-3 font-semibold truncate">
+                  {" "}
+                  Student Name
+                </th>
+                <th className="px-4 py-3 font-semibold truncate">
+                  {" "}
+                  Course Title
+                </th>
+                <th className="px-4 py-3 font-semibold truncate"> Date</th>
+              </tr>
+            </thead>
+            <tbody className="text-sm text-gray-500">
+              {data.map((item, index) => (
+                <tr key={index} className="border-b border-gray-500/20">
+                  <td className="px-4 py-3 text-center hidden sm:table-cell">
+                    {index + 1}
+                  </td>
+                  <td className="md:px-4 px-3 py-3 flex items-center space-x-3">
+                    <img
+                      src={item.student.imageUrl}
+                      alt=""
+                      className="w-9 h-9 rounded-full"
+                    />
+                    <span className="truncate">{item.student.name}</span>
+                  </td>
+                  <td className="px-4 py-3 truncate">{item.courseTitle}</td>
+                  <td className="px-4 py-3 hidden sm:table-cell">{new Date(item.purchaseDate).toLocaleDateString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default StudentEnrolled;
