6445559a6a8bff246c30e9a2b9561e7516d56e97
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-04-07 21:48:50 +0530

    created user Schema

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.SchemaType(
+    {
+        _id : {
+            type : String,
+            required : true
+        },
+        name:{
+            type : String,
+            required : true,
+        },
+        email:{
+            type : String,
+            required : true,
+        },
+        imageUrl : {
+            type : String,
+            required : true,
+        },
+        enrolledCourses : [
+            {
+                type : mongoose.SchemaType.Types.ObjectId,
+                ref : 'Course'
+            }
+        ],
+    } , { timestamps : true}
+);
+
+
+const User = mongoose.model('User' , userSchema);
+
+export default User;
+
